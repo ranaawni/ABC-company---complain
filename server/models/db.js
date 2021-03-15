c3c028c269ca23c372fa5d1170886f4ced1066a3
Rana Alhmoud <ranaalhmoud92@gmail.com>
2021-03-15 05:19:07 +0200

    connect with database

--- server/models/db.js
+++ server/models/db.js
@@ -1,1 +1,15 @@
 const mysql = require("mysql");
+const connection = mysql.createPool({
+    host: "us-cdbr-east-03.cleardb.com",
+    user: "b1975620186c36",
+    password: "7aa4ae51",
+    database: "heroku_b727c40cfeb9a9e",
+  });
+  
+  connection.query("select 1 + 1", (err, rows) => {
+    if (err) throw err;
+    console.log("Connected! , hello");
+  });
+  
+  module.exports = connection;
+  
